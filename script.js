51e23c4f216557f289d7bd6cd9bfd13f291ee049
guicalmeida <guica.almeida@gmail.com>
2020-12-26 11:09:33 -0300

    game started upon button click

--- script.js
+++ script.js
@@ -21,21 +21,23 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-while (playerScore !== 5 && computerScore !== 5) {
-    let player;
-    let answer;
-    while ((player !== "rock") && (player !== "paper") && (player !== "scissors")) {
-        answer = prompt("rock, paper, scissors?");
-        player = answer.toLowerCase();
-       }
-    console.log(playRound(player, computerPlay()));
-    console.log ("your score: " + playerScore);
-    console.log ("A.I. score: " + computerScore);
-    console.log ("%cNext round...", "color:grey; font size: 20px; font-style: italic")
-    if (playerScore == 5) {
-        console.log("%cYou Win the Game!", "color:red; font-size: 30px");
-    }
-    else if (computerScore == 5) {
-        console.log("%cYou Lost the Game!", "color:blue; font-size: 30px");
+function game() {
+    while (playerScore !== 5 && computerScore !== 5) {
+        let player;
+        let answer;
+        while ((player !== "rock") && (player !== "paper") && (player !== "scissors")) {
+            answer = prompt("rock, paper, scissors?");
+            player = answer.toLowerCase();
+        }
+        console.log(playRound(player, computerPlay()));
+        console.log ("your score: " + playerScore);
+        console.log ("A.I. score: " + computerScore);
+        console.log ("%cNext round...", "color:grey; font size: 20px; font-style: italic")
+        if (playerScore == 5) {
+            console.log("%cYou Win the Game!", "color:red; font-size: 30px");
+        }
+        else if (computerScore == 5) {
+            console.log("%cYou Lost the Game!", "color:blue; font-size: 30px");
+        }
     }
 }
